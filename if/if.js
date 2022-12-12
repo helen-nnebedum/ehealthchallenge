1eedc625d2e4f0daaa2510b0c3e0c4a52890de32
helen-nnebedum <118439018+helen-nnebedum@users.noreply.github.com>
2022-12-12 18:05:30 +0530

    Add files via upload

---
+++ if/if.js
@@ -0,0 +1,14 @@
+let score = 60;
+let letter;
+
+if (score >=70) {
+  letter = 'A';
+}
+else if (score >=50 ) {
+  letter = 'C';
+}
+else {
+  letter = 'F';
+}
+
+console.log('You got the letter grade ' + letter );
